55cf750b55b9ffc7638add6a7a70ddd4c4920fbd
Apil <156303310+apilpirmangithub@users.noreply.github.com>
2025-11-11 00:05:08 +0700

    Create index.ts to export components and types

---
+++ client/components/remix-mode /index.ts
@@ -0,0 +1,9 @@
+export { PopularIPGrid } from "./PopularIPGrid";
+export { AddRemixImageModal } from "./AddRemixImageModal";
+export { RemixImage } from "./RemixImage";
+export type {
+  PopularItem,
+  PreviewImage,
+  PreviewImagesState,
+  SearchResult,
+} from "./types";
