999d9749fbdc2d2b0232f1ee5047a51df0e33c80
Apil <156303310+apilpirmangithub@users.noreply.github.com>
2025-11-11 00:12:41 +0700

    Delete client/components/remix-mode  directory

--- client/components/remix-mode /index.ts
+++
@@ -1,9 +0,0 @@
-export { PopularIPGrid } from "./PopularIPGrid";
-export { AddRemixImageModal } from "./AddRemixImageModal";
-export { RemixImage } from "./RemixImage";
-export type {
-  PopularItem,
-  PreviewImage,
-  PreviewImagesState,
-  SearchResult,
-} from "./types";
