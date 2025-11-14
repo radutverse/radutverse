e4d35d479bcd25361971d8df20ddae239800671d
radutverse <radutverse@gmail.com>
2025-11-14 17:44:07 +0000

    delete old structure after migration

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import { createServer } from "../server/index.js";
-
-const app = createServer();
-
-export default function handler(req: any, res: any) {
-  // Pass the request directly to Express. This works with Vercel Node functions.
-  return app(req, res);
-}
---
+++ packages/shared/src/index.ts
@@ -0,0 +1,11 @@
+// Shared exports
+
+export * from './types/generation';
+export * from './utils/apply-watermark';
+export * from './utils/crypto';
+export * from './utils/hash';
+export * from './utils/ipfs';
+export * from './utils/perceptual-hash';
+export * from './utils/vision-api';
+export * from './utils/watermark';
+export * from './constants/ip-assistant';
