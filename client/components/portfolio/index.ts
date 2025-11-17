ee50218bf7dc03150c378f0aa9fd77cab3fbcbb7
radutverse <radutverse@gmail.com>
2025-11-17 16:40:24 +0700

    Create index.ts

---
+++ client/components/portfolio/index.ts
@@ -0,0 +1,4 @@
+export { ConnectWalletView } from "./ConnectWalletView";
+export { BalanceCard } from "./BalanceCard";
+export { IpAssetsGrid, type IpAsset } from "./IpAssetsGrid";
+export { PortfolioHeader } from "./PortfolioHeader";
